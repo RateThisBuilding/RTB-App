b406247c73e8515d56ec4891d680b157d6899e80
Jack Chao <euleriepsilon@gmail.com>
2016-11-04 16:57:13 -0700

    Removed useless UI saga

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export default function (state = initialState, action){
   switch(action.type){
-    case actionTypes.OPEN_GLOBAL_MODAL_SUCCESS:
+    case actionTypes.OPEN_GLOBAL_MODAL:
       return {...state, globalModal: true}
     case actionTypes.CLOSE_GLOBAL_MODAL:
       return {...state, globalModal: false}
